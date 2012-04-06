7130e63770cd634baa0289f8b0a1c9bf90a9d3e8
Abhishek Srinath <asrinath@buffalo.edu>
2012-04-06 13:39:37 -0400

    Adding the project folder

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: abhishek-srinath
+ *
+ * Created on April 6, 2012, 1:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
